aac39ecdb9c8e686818dd521f0b78b70378bb7bf
kymppi <me@midka.dev>
2022-02-01 20:35:35 +0200

    init prisma & tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return <div className="container bg-pink-400 rounded-3xl p-52 mx-auto"></div>;
+};
+
+export default Home;
