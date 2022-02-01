09d9a0932e6ba2c8ba87c64c869ca2ff9ba6a8e3
kymppi <me@midka.dev>
2022-02-01 21:12:15 +0200

    starting layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,17 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
-  return <div className="container bg-pink-400 rounded-3xl p-52 mx-auto"></div>;
+  return (
+    <div className="w-full h-screen">
+      <div className="relative h-64 w-96">
+        <Image
+          src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
